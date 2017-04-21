82109dba1c4cefdb3355fac3c3bd40d26a31c0c0
s.garmaev <garmsayan@gmail.com>
2017-04-21 18:58:16 +0300

    Try DOM4J

--- src/main/java/rozetkautils/EmailUtilities.java
+++ src/main/java/rozetkautils/EmailUtilities.java
@@ -1,7 +1,15 @@
 package rozetkautils;
 
+import org.cyberneko.html.parsers.DOMParser;
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.DOMReader;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import javax.mail.*;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.Session;
@@ -10,30 +18,50 @@ import javax.mail.MessagingException;
 
 
 public class EmailUtilities {
+    private static Message[] messages;
     public static Message[] getEmailMessages(String host, String user, String pass) throws MessagingException, IOException {
         Auth auth=new Auth(user,pass);
-        Message[] messages;
         Properties props = new Properties();
-        props.put("mail.debug", "true");
-        props.setProperty("mail.store.protocol", "imaps");
-        Session session = Session.getDefaultInstance(props, auth);
-        //IMAPStore emailStore = (IMAPStore) session.getStore("imaps");
-        //emailStore.connect(user, pass);
-        Store emailStore = session.getStore("imaps");
-        emailStore.connect(host, user, pass);
+        props.put("mail.pop3.host", host);
+        props.put("mail.pop3.port", "995");
+        props.put("mail.pop3.starttls.enable", "true");
+        Session emailSession = Session.getDefaultInstance(props);
 
-        Folder inbox = emailStore.getFolder("inbox");
+        Store store = emailSession.getStore("pop3s");
+        store.connect(host, user, pass);
+
+        Folder inbox = store.getFolder("Inbox");
         inbox.open(Folder.READ_ONLY);
-
         messages = inbox.getMessages();
-
+/*
         //получаем последнее сообщение (самое старое будет под номером 1)
         Message m = inbox.getMessage(inbox.getMessageCount()-1);
         Multipart mp = (Multipart) m.getContent();
-        BodyPart bp = mp.getBodyPart(0);
-
-        //Выводим содержимое на экран
-        System.out.println(bp.getContent());
+        BodyPart bp = mp.getBodyPart(0);*/
         return messages;
+    }
+
+    public static Message getEmailMessage(String email, String password, String fromAddress, String subject) throws IOException, MessagingException {
+        messages = getEmailMessages("pop.mail.ru", email, password);
+        for(Message msg:messages){
+            if(msg.getFrom()[0].toString().contains(fromAddress)&&(msg.getSubject().equals(subject))){
+                return msg;
+            }
+        }
+        return null;
+    }
+
+    public static void clickOnConfirmRegistration(Message msg) throws IOException, SAXException {
+        DOMParser parser = new DOMParser();
+        parser.parse(String.valueOf(msg));
+        org.w3c.dom.Document doc = parser.getDocument();
+
+        DOMReader reader = new DOMReader();
+        Document document = reader.read(doc);
+
+        @SuppressWarnings("unchecked")
+        List<Node> nodes = document.selectNodes("//span[contains(text(),'перейдите по ссылке')]/ancestor::a");
+        nodes.get(0).getStringValue()
+
     }
 }
--- src/test/java/AuthenticationTests.java
+++ src/test/java/AuthenticationTests.java
@@ -1,6 +1,7 @@
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
 import rozetkapages.HomePage;
 import rozetkapages.LoginPopUp;
 import rozetkapages.PersonalDataPage;
@@ -15,7 +16,7 @@ import java.lang.reflect.Array;
 
 
 public class AuthenticationTests extends BasicTestCase {
-    private HomePage homePage=new HomePage(getWebDriver());
+    private HomePage homePage = new HomePage(getWebDriver());
     private LoginPopUp loginPopUp;
     private SignUpPage signUpPage;
     private PersonalDataPage personalDataPage;
@@ -27,16 +28,22 @@ public class AuthenticationTests extends BasicTestCase {
     }
 
     @Test
-    public void checkEmailNotificationOfRegistration() throws IOException, MessagingException {
-        Message[] messages;
-        signUpPage=homePage.openLoginPopUp().openSignUpPage();
-        signUpPage.enterUserName("Uidddfiii");
-        signUpPage.enterUserEmail("hsjddhdkdjshds@gmail.com");
-        signUpPage.enterUserPassword("Qwerty1Q");
-        personalDataPage=signUpPage.clickOnRegistration();
-        Assert.assertEquals(personalDataPage.getTitlePage(),"Личные данные", "Incorrect page title");
-        messages=EmailUtilities.getEmailMessages("imap.mail.ru", "tserg_2011@mail.ru", "Rfhfylfitkm");
-
+    public void checkEmailNotificationOfRegistration() throws IOException, MessagingException, SAXException {
+        Message notifMsg;
+        String email="tserg_2011@mail.ru";
+        String password="Rfhfylfitkm";
+        String userName="UserName";
+        signUpPage = homePage.openLoginPopUp().openSignUpPage();
+        signUpPage.enterUserName(userName);
+        signUpPage.enterUserEmail(email);
+        signUpPage.enterUserPassword(password);
+        personalDataPage = signUpPage.clickOnRegistration();
+        Assert.assertEquals(personalDataPage.getTitlePage(), "Личные данные", "Incorrect page title");
+        notifMsg=EmailUtilities.getEmailMessage(email,password,"<sales@rozetka.com.ua>","Подтверждение регистрации");
+        Assert.assertTrue(!notifMsg.equals(null), "Email notification isn't sent");
+        Assert.assertTrue(notifMsg.getContent().toString().contains(password),"Нет пароля в письме!");
+        Assert.assertTrue(notifMsg.getContent().toString().contains(email),"Нет логина в письме!");
+        EmailUtilities.clickOnConfirmRegistration(notifMsg);
     }
 
 }
